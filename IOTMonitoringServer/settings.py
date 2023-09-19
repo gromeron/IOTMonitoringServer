24ea5acecf289ac60e7019a1cdffa48da6f00cff
Gustavo Romero <gustavo.romero@pragma.com.co>
2023-09-19 16:37:27 -0500

    tutorial ips modified_v6

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.83.113.220"]
+ALLOWED_HOSTS = ["localhost", "52.23.157.68"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.164.41.255",  # Dirección IP de la base de datos
+        "HOST": "34.228.197.103",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.86.17.208"
+MQTT_HOST = "52.207.250.176"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
